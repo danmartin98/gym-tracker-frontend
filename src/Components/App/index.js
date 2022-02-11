581732489abdf2b30f21d47d0315831dcd054ace
Antonio Riccelli <anton.riccelli@gmail.com>
2022-02-11 10:46:14 +0000

    Installed react-router-dom, set up first base route

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import {Routes, Route, Link} from "react-router-dom";
+import LoginPage from "../PAGES/LoginPage";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+  <div className="App">Hello
+  <Routes>
+    <Route path="/" element={<LoginPage />}/>
+  </Routes>
+
+
+  </div>
+  )
 }
 
 export default App;
