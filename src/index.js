d4f65789353ac6af7217f23e5df4c8ae537ba2aa
Antonio Riccelli <anton.riccelli@gmail.com>
2022-02-11 10:54:45 +0000

    Installed and implemented Auth0Provider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "../src/Components/App";
+import {Auth0Provider} from "@auth0/auth0-react";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
+  <Auth0Provider
+  domain="dev-aqqete6x.us.auth0.com"
+  clientId="sSsOfSEhL1qpIKDrYwaanR8vDYS2QAUR"
+  redirectUri={window.location.origin}
+  >
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Auth0Provider>
+  ,
   document.getElementById("root")
 );
